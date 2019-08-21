177d5465252036ca585b8fa42761397a78fb2bf8
Sangam Khanal <sangamkhnl@gmail.com>
2019-08-21 15:35:14 +0545

    Scope issue fixed

--- EvaluateValue.C
+++ EvaluateValue.C
@@ -9,8 +9,8 @@ EvaluateValue::EvaluateValue(char* input_str)
     {
         std::stringstream string_stream1, string_stream2;
         input_str++;
-        EvaluateValue::to = *input_str;
-        EvaluateValue::from = *(input_str+2);
+        to = *input_str;
+        from = *(input_str+2);
         input_str = input_str + 4;
         while(*input_str!=',')
         {   
@@ -19,7 +19,7 @@ EvaluateValue::EvaluateValue(char* input_str)
             input_str++;
         }     
         //std::cout << string_stream.str()<<std::endl;
-        string_stream1 >> EvaluateValue::rate;  
+        string_stream1 >> rate;  
         //string_stream.str("");    
         //std::cout<<*input_str<<std::endl;
         input_str++;
@@ -29,7 +29,7 @@ EvaluateValue::EvaluateValue(char* input_str)
             string_stream2 << *input_str;
             input_str++;
         }
-        string_stream2 >> EvaluateValue::time;
+        string_stream2 >> time;
         string_stream2.str("");
         
     }
@@ -43,81 +43,81 @@ EvaluateValue::EvaluateValue(char* input_str)
 }
 void EvaluateValue::Printall()
 {
-    std::cout << EvaluateValue::to<<std::endl;
-    std::cout << EvaluateValue::from<<std::endl;
-    std::cout << EvaluateValue::rate<<std::endl;
-    std::cout << EvaluateValue::time<<std::endl;
+    std::cout <<to<<std::endl;
+    std::cout <<from<<std::endl;
+    std::cout <<rate<<std::endl;
+    std::cout <<time<<std::endl;
 }
 float EvaluateValue::Calculate()
 {
-    if(EvaluateValue::to =='P' || EvaluateValue::to =='p')
+    if(to =='P' || to =='p')
     {
-        if(EvaluateValue::from == 'f' || EvaluateValue::from == 'F')
+        if(from == 'f' || from == 'F')
         {
-            double denominator = 1+EvaluateValue::rate;
+            double denominator = 1+rate;
             denominator = pow(denominator,time);
             double numerator = 1;
             return numerator/denominator;
            
         }
-        if(EvaluateValue::from == 'a' || EvaluateValue::from == 'A')
+        if(from == 'a' || from == 'A')
         {
-            double val = pow(1+EvaluateValue::rate,time);
+            double val = pow(1+rate,time);
             double numerator = val-1;
             double denominator = rate*val;
             return numerator/denominator;
         }
 
-        if(EvaluateValue::from == 'g' || EvaluateValue::from == 'G')
+        if(from == 'g' || from == 'G')
         {
-            double val = pow(1+EvaluateValue::rate,time);
+            double val = pow(1+rate,time);
             double numerator = val -rate*time-1;
             double denominator = rate*rate *val;
             return numerator/denominator;
         }
     }
-    if(EvaluateValue::to =='f' || EvaluateValue::to =='F')
+    if(to =='f' || to =='F')
     {
-        if(EvaluateValue::from == 'p' || EvaluateValue::from == 'P')
+        if(from == 'p' || from == 'P')
         {
-            double val = pow(1+EvaluateValue::rate,time);
+            double val = pow(1+rate,time);
             return val;
         }
-        if(EvaluateValue::from == 'a' || EvaluateValue::from == 'A')
+        if(from == 'a' || from == 'A')
         {
-            double val = pow(1+EvaluateValue::rate,time);
+            double val = pow(1+rate,time);
             double numerator = val-1;
             double denominator = rate;
             return numerator/denominator;
         }
-        if(EvaluateValue::from == 'g' || EvaluateValue::from == 'G')
+        if(from == 'g' || from == 'G')
         {
             std::cout<<"sorry"<<std::endl;
             return 0;
         }
     }
 
-    if(EvaluateValue::to =='a' || EvaluateValue::to =='A')
+    if(to =='a' || to =='A')
     {
-        if(EvaluateValue::from == 'p' || EvaluateValue::from == 'P')
+        if(from == 'p' || from == 'P')
         {
-            double val = pow(1+EvaluateValue::rate,time);
+            double val = pow(1+rate,time);
             double numerator = rate*val;
             double denominator = val-1;
             return numerator/denominator;
 
         }
-        if(EvaluateValue::from == 'f' || EvaluateValue::from == 'F')
+        if(from == 'f' || from == 'F')
         {
-            double val = pow(1+EvaluateValue::rate,time);
+            double val = pow(1+rate,time);
             double numerator = rate;
             double denominator = val-1;
             return numerator/denominator;
         }
 
-        if(EvaluateValue::from == 'g' || EvaluateValue::from == 'G')
+        if(from == 'g' || from == 'G')
         {
-            double val = pow(1+EvaluateValue::rate,time);
+            double val = pow(1+rate,time);
             double numerator = val-rate*time-1;
             double denominator = rate*val-rate;
             return numerator/denominator;
